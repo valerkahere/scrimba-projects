0881d00fbe716fb587030254d6b099ec9ac72ec0
Valerii Navalnyi <adultvalerka@gmail.com>
2025-11-01 21:04:37 +0000

    4) Passenger Counter App - Finished the program in my own way, ready to refactor after finishing the course.

--- making-websites-interactive/01_Passenger-Counter-App/index.js
+++ making-websites-interactive/01_Passenger-Counter-App/index.js
@@ -4,15 +4,50 @@ let countEl = document.getElementById("count-el");
 (() => {
     
 
-    const btn = document.getElementById("increment-btn");
+    const incrementBtn = document.getElementById("increment-btn");
     // If the element not null, the if statement will be executed
-    if (btn) {
-        btn.addEventListener("click", Increment)
+    if (incrementBtn) {
+        incrementBtn.addEventListener("click", increment)
+    }
+
+    const saveBtn = document.getElementById("save-btn");
+    if (saveBtn) {
+        saveBtn.addEventListener("click", save)
     }
 
 })();
 
-function Increment(event) {
+function increment(event) {
     count+=1;
     countEl.innerText = count;
+}
+
+function save(event) {
+
+    
+    let saved = document.getElementById("saved");
+
+    // If there is a dot, it's removed
+    if (saved.textContent.endsWith(".")) {
+        saved.textContent = saved.textContent.slice(0, -1);
+    }
+
+
+    
+    
+    // Checking that save button wasn't cliked yet, so different display method is used
+    const savedSplit = saved.innerText.split(" ");
+    // [2] because this is where number start to appear
+    if (!(parseInt(savedSplit[2]))) {
+        
+        saved.innerText += ` ${count}.`;
+        
+    } 
+    else {
+        saved.innerText += `, ${count}.`;
+    }
+    
+    // Когда сейв снова нажимается
+    // Убрать точку, если она есть в конце
+    
 }
