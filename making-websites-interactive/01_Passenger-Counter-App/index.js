b60aea972b013cb25ff95939244d6fc8752cfabc
Valerii Navalnyi <adultvalerka@gmail.com>
2025-11-01 16:33:28 +0000

    1) Passenger Counter App - Implemented basic counter functioning

--- making-websites-interactive/01_Passenger-Counter-App/index.js
+++ making-websites-interactive/01_Passenger-Counter-App/index.js
@@ -1,1 +1,20 @@
-document.getElementById("count-el").innerText = 5
+(() => {
+    // On page reload, reset the value
+    document.getElementById("count-el").innerText = 0;
+    
+
+    const btn = document.getElementById("increment-btn");
+    // If the element not null, the if statement will be executed
+    if (btn) {
+        btn.addEventListener("click", Increment)
+    }
+
+})();
+
+function Increment(event) {
+    const counter = document.getElementById("count-el");
+    const counterCurrentValue = counter.innerText;
+    document.getElementById("count-el").innerText = "";
+    counter.innerText += (parseInt(counterCurrentValue) + parseInt("1"));
+
+}
