c3f5c0b13879d6bbdbc827a958d05c16776f7bb5
Valerii Navalnyi <adultvalerka@gmail.com>
2026-01-27 13:48:05 +0000

    Change content path, add 2 more posts

--- astro/keystatic.config.ts
+++ astro/keystatic.config.ts
@@ -8,7 +8,7 @@ export default config({
     posts: collection({
       label: 'Posts',
       slugField: 'title',
-      path: 'src/content/posts/*',
+      path: 'src/content/posts/*/',
       format: { contentField: 'content' },
       schema: {
         title: fields.slug({ name: { label: 'Title' } }),
